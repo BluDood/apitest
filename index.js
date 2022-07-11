f31ded3725080e5430da03b110626a8395027f29
BluDood <bludood@bludood.com>
2022-07-11 16:24:09 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+    console.log('request')
     res.send('Hello World!')
 })
 
-app.listen(8080)
+app.listen(8080, () => {
+    console.log('started')
+})
